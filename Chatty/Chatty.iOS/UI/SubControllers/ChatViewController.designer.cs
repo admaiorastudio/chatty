6fcddc5ca4b189ce9d0a65249c5819a4ab1d617f
Francesco Colombo <francesco.colombo@admaiorastudio.com>
2016-10-18 17:12:05 +0200

    iOS: working on UI

--- Chatty/Chatty.iOS/UI/SubControllers/ChatViewController.designer.cs
+++ Chatty/Chatty.iOS/UI/SubControllers/ChatViewController.designer.cs
@@ -1,18 +1,50 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
-namespace AdMaiora.Chatty.UI.SubControllers
+namespace AdMaiora.Chatty
 {
-    [Register("ChatViewController")]
-    partial class ChatViewController
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("ChatViewController")]
+	partial class ChatViewController
+	{
+		[Outlet]
+		UIKit.UIView BackLayout { get; set; }
+
+		[Outlet]
+		UIKit.UIView InputLayout { get; set; }
+
+		[Outlet]
+		AdMaiora.AppKit.UI.UIItemListView MessageList { get; set; }
+
+		[Outlet]
+		UIKit.UITextView MessageText { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (MessageList != null) {
+				MessageList.Dispose ();
+				MessageList = null;
+			}
+
+			if (InputLayout != null) {
+				InputLayout.Dispose ();
+				InputLayout = null;
+			}
+
+			if (MessageText != null) {
+				MessageText.Dispose ();
+				MessageText = null;
+			}
+
+			if (BackLayout != null) {
+				BackLayout.Dispose ();
+				BackLayout = null;
+			}
+		}
+	}
 }
